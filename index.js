7fd0f713fbfa357914b3a52c3af8c000667e518d
Your Name <you@example.com>
2021-03-23 00:23:57 +0600

    first node-mongo

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/' ,(req, res) => {
+    res.send("Thanks for Calling Khaled Shourov")
+})
+
+app.listen(3000, () => console.log('Listentiong to port 3000'));
